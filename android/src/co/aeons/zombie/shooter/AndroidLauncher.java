8a84deef3b678f617903c9a8511692271707def8
Torstein Molland <rex.datamaskin@gmail.com>
2018-04-14 14:21:49 +0200

    Add libgdx stash

---
+++ android/src/co/aeons/zombie/shooter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package co.aeons.zombie.shooter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import co.aeons.zombie.shooter.ZombieShooter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ZombieShooter(), config);
+	}
+}
---
+++ desktop/src/co/aeons/zombie/shooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package co.aeons.zombie.shooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import co.aeons.zombie.shooter.ZombieShooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZombieShooter(), config);
+	}
+}
