5d0b62084f802d6c865087057e6cc10356ab7dff
Håvard Opheim <haavard@vaage.com>
2018-04-17 15:54:48 +0200

    Add the rest

--- core/src/co/aeons/zombie/shooter/entities/buttons/NukeButton.java
+++ core/src/co/aeons/zombie/shooter/entities/buttons/NukeButton.java
@@ -1,4 +1,33 @@
 package co.aeons.zombie.shooter.entities.buttons;
 
-public class NukeButton {
+import com.badlogic.gdx.math.Rectangle;
+
+import java.util.ArrayList;
+
+import co.aeons.zombie.shooter.entities.Zombie;
+import co.aeons.zombie.shooter.gamestates.PlayState;
+
+public class NukeButton extends EffectButton{
+
+    public NukeButton(Rectangle bounds) {
+        super(bounds);
+    }
+
+    //Nuke effect: kill all zombies on screen
+    public void effect(ArrayList<Zombie> zombies) {
+        zombies.clear();
+}
+
+    @Override
+    protected String getTexturePath() {
+        return "buttons/nukeButton.png";
+    }
+
+    @Override
+    public void effect(PlayState playState) {
+
+        playState.getZombies().clear();
+
+    }
+
 }
